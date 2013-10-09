643c4ac8a05c5914f73b0e31b56c0d8a55393495
Steve Traylen <steve.traylen@cern.ch>
2013-10-09 22:47:57 +0200

    puppet lint fixes.

--- Rakefile
+++ Rakefile
@@ -1,6 +1,7 @@
 require 'rake'
 
 require 'puppet-lint/tasks/puppet-lint'
+PuppetLint.configuration.send('disable_class_parameter_defaults')
 require 'rspec/core/rake_task'
 
 RSpec::Core::RakeTask.new(:spec) do |t|
--- manifests/service.pp
+++ manifests/service.pp
@@ -1,43 +1,44 @@
+#Class: fts::service
 class fts::service {
 
-    include ('fetchcrl')
+  include ('fetchcrl')
 
-    service{'fts-server':
-       enable    => true,
-       ensure    => running,
-       subscribe => Package['fts-server']       
-    }
-    service{'fts-msg-bulk':
-       enable => true,
-       ensure => running,
-       subscribe => Package['fts-server']
-    }
-    service{'fts-records-cleaner':
-       enable => true,
-       ensure => running,
-    }
-    service{'fts-msg-cron':
-       enable => true,
-       ensure => running,
-    }
-    service{'fts-bdii-cache-updater':
-       enable => true,
-       ensure => running,
-    }
-    service{'fts-bringonline':
-       enable => true,
-       ensure => running,
-       subscribe => Package['fts-server']
-    }                
-    service{'httpd':
-       enable => true,
-       ensure => running,
-       subscribe => [Package['fts-rest'], Class['fetchcrl']]
-   } 
+  service{'fts-server':
+    ensure    => running,
+    enable    => true,
+    subscribe => Package['fts-server']
+  }
+  service{'fts-msg-bulk':
+    ensure    => running,
+    enable    => true,
+    subscribe => Package['fts-server']
+  }
+  service{'fts-records-cleaner':
+    ensure => running,
+    enable => true,
+  }
+  service{'fts-msg-cron':
+    ensure => running,
+    enable => true,
+  }
+  service{'fts-bdii-cache-updater':
+    ensure => running,
+    enable => true,
+  }
+  service{'fts-bringonline':
+    ensure    => running,
+    enable    => true,
+    subscribe => Package['fts-server']
+  }
+  service{'httpd':
+    ensure    => running,
+    enable    => true,
+    subscribe => [Package['fts-rest'], Class['fetchcrl']]
+  }
 
-   service{['bdii','fts-info-publisher']:
-       enable => true,
-       ensure => running,
-   }
+  service{['bdii','fts-info-publisher']:
+    ensure => running,
+    enable => true,
+  }
 }
 
